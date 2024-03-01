2c9a4a74626ec3bc5dc309a11f98ed86a2133c90
Lucas Cardoso <Luckycards433@gmail.com>
2024-03-01 10:05:01 -0300

    Title created and animation progress 2

---
+++ src/components/bubbles/index.tsx
@@ -0,0 +1,9 @@
+import style from "./style.module.scss"
+
+export default function Bubbles() {
+  return (
+    <div className={style.container}>
+      <span className={style.bubble}/>
+    </div>
+  )
+}
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,12 +1,18 @@
-import style from "./style.module.scss"
+import style from "./style.module.scss";
+import Bubbles from "../../components/bubbles";
 
 export default function Home() {
   return (
     <>
       <div className={style.container}>
-        <h1>Lucas Cardoso</h1>
-        <h1 className={style.animate}>Lucas Cardoso</h1>
+        <div className={style.title}>
+          <h1>Lucas Cardoso</h1>
+          <h1 className={style.animate}>Lucas Cardoso</h1>
+        </div>
+        <div className={style.teste}>odawodwadwad</div>
       </div>
+      <Bubbles />
+
     </>
   );
 }
