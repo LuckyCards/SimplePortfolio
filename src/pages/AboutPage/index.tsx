89eebca1f312d2e14e0106fd042e8b665a6db6c0
Lucas Cardoso <Luckycards433@gmail.com>
2024-03-09 01:07:57 -0300

    mudada a descrição do projeto

--- src/pages/AboutPage/index.tsx
+++ src/pages/AboutPage/index.tsx
@@ -6,8 +6,15 @@ export default function AboutPage() {
       <main className={style.container}>
         <div className={style.about}>
           <h3>Quem Somos?</h3>
-          <p>O Instituto Marítimo é uma instituição dedicada à paixão pelo mundo subaquático. Nossos objetivos são:</p>
-          <p>Instruir Jovens Mergulhadores: Compartilhamos conhecimento e habilidades com jovens entusiastas, capacitando-os a explorar as profundezas dos oceanos com segurança e confiança.</p>
+          <p>
+            O Instituto Marítimo é uma instituição dedicada à paixão pelo mundo
+            subaquático. Nossos objetivos são:
+          </p>
+          <p>
+            Instruir Jovens Mergulhadores: Compartilhamos conhecimento e
+            habilidades com jovens entusiastas, capacitando-os a explorar as
+            profundezas dos oceanos com segurança e confiança.
+          </p>
         </div>
         <div className={style.feedback}></div>
       </main>
